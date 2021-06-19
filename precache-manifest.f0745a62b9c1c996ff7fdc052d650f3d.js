42aeb014c1535a2ef8c09f1c5d576743dcd22b66
Nandini Jaryal <70465552+enjae@users.noreply.github.com>
2021-06-19 15:29:43 +0530

    Updates

---
+++ precache-manifest.f0745a62b9c1c996ff7fdc052d650f3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc4941e109ce7641ce2b",
+    "url": "/ethe_scan/static/css/main.1fd490c9.chunk.css"
+  },
+  {
+    "revision": "bc4941e109ce7641ce2b",
+    "url": "/ethe_scan/static/js/main.bc4941e1.chunk.js"
+  },
+  {
+    "revision": "c221a82a77883d65811d",
+    "url": "/ethe_scan/static/css/1.58223cd6.chunk.css"
+  },
+  {
+    "revision": "c221a82a77883d65811d",
+    "url": "/ethe_scan/static/js/1.c221a82a.chunk.js"
+  },
+  {
+    "revision": "97201b6cc83bf01e78ca",
+    "url": "/ethe_scan/static/js/runtime~main.97201b6c.js"
+  },
+  {
+    "revision": "c11c9167d0c991e41db4f6927d755026",
+    "url": "/ethe_scan/static/media/etherium.c11c9167.png"
+  },
+  {
+    "revision": "92f807e1ab801041604a8f8c2bcfb8eb",
+    "url": "/ethe_scan/index.html"
+  }
+];
